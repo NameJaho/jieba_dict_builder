5a6becaee2f0bb60460729346031e2f3b52730ed
liujeru <jeru.token@gmail.com>
2024-08-29 10:37:23 +0800

    add new_word_detector.py

---
+++ mi_calculator.py
@@ -0,0 +1,58 @@
+import utils
+import pandas as pd
+import math
+
+CONFIG_FILE = 'config/config.yaml'
+CHAR_FREQ_FILE = 'output/char_freq.csv'
+WORD_FREQ_FILE = 'output/word_freq.csv'
+
+
+class MICalculator:
+    def __init__(self):
+        config = utils.load_config(CONFIG_FILE)
+        self.blacklist = config['BLACKLIST']
+        self.df_char_freq = pd.read_csv(CHAR_FREQ_FILE)
+        self.df_word_freq = pd.read_csv(WORD_FREQ_FILE)
+
+    def find_char_frequency(self, char):
+        df = self.df_char_freq
+        # 找到对应的词频
+        frequency = df.loc[df['single_char'] == char, 'count'].iloc[0] if char in df['single_char'].values else 0
+        return frequency
+
+    def find_word_frequency(self, word):
+        df = self.df_word_freq
+        # 找到所有包含输入词的词频之和
+        frequency = df[df['ngram'].str.contains(word, regex=False)]['count'].sum()
+        return frequency
+
+    def calculate_mutual_information(self, term):
+        term_freq = self.find_word_frequency(term)
+
+        # 获取词的每个字符
+        chars = list(term)
+
+        # 计算总频率
+        total_freq = term_freq
+        char_freq_dict = {}
+
+        # 获取每个字符的全局词频
+        for char in chars:
+            char_freq = self.find_char_frequency(char)
+            if char_freq is not None:
+                char_freq_dict[char] = char_freq
+                total_freq += char_freq
+
+        if total_freq == 0 or term_freq == 0:
+            return 0
+
+        # 计算概率
+        p_term = term_freq / total_freq
+        p_chars = {char: freq / total_freq for char, freq in char_freq_dict.items()}
+
+        # 计算互信息 判断凝固度
+        mi = math.log(p_term / math.prod(p_chars.values()), 2)
+
+        #print(f"Mutual information for term '{term}': {mi}")
+        return mi
+
--- new_word_detector.py
+++ new_word_detector.py
@@ -1,4 +1,49 @@
+import pandas as pd
+import polars as pl
+from utils import cost_time
+
+from entropy_calculator import EntropyCalculator
+from mi_calculator import MICalculator
+
+TERMS_FILE = 'output/terms_data.csv'
+ENTROPY_RESULT_FILE = 'output/entropy_result.csv'
+
+
 class NewWordDetector:
     def __init__(self):
+        self.entropy_calculator = EntropyCalculator()
+        self.mi_calculator = MICalculator()
+
+    @cost_time
+    def filter_with_entropy(self):
+        entropy_results = []
+        df = pl.read_csv(TERMS_FILE)
+
+        for row in df.iter_rows(named=True):
+            term = row['term']
+            term_freq = row['term_freq']
+            doc_freq = row['doc_freq']
+            left_chars = eval(row['left_chars'])
+            right_chars = eval(row['right_chars'])
+
+            entropy = self.entropy_calculator.calculate_entropy(left_chars, right_chars)
+
+            if entropy >= 1.5 and len(term.strip()) > 1:
+                entropy_results.append({'term': term, 'term_freq': term_freq, 'doc_freq': doc_freq})
+
+        return entropy_results
+
+    @cost_time
+    def save_entropy_results(self):
+        entropy_results = self.filter_with_entropy()
+        df = pl.DataFrame(entropy_results)
+        df.write_csv(ENTROPY_RESULT_FILE, separator=",")
+
+    def filter_with_mi(self):
         pass
+
+
+if __name__ == '__main__':
+    new_word_detector = NewWordDetector()
+    new_word_detector.save_entropy_results()
 
