c89ad436c3ea0acb872b5fec52081e7d65fec36f
Jaho <>
2024-09-04 19:12:29 +0800

    优化

--- word_discoverer_chunk.py
+++ word_discoverer_chunk.py
@@ -23,7 +23,7 @@ class WordDiscoverer(ConfigLoader):
 
     @cost_time
     def process(self):
-        chunksize = 20 ** 6  # 每次读取100万行
+        chunksize = 10 ** 6  # 每次读取100万行
         chunks = pd.read_csv('xhs_3000w.csv', chunksize=chunksize)
 
         # chunks = pd.read_csv(self.input_file_path.input_file, chunksize=chunksize)
